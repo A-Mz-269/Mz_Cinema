99007228725886215df531fa24a1403a6adf5433
A-Mz-269 <meziabeabdeldjalil27@gmail.com>
2026-02-06 06:14:40 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,26 @@
-const CACHE_NAME = 'mz-cinema-v1';
+const CACHE_NAME = 'mz-cinema-v2';
 const ASSETS = [
     './',
     './index.html',
-    'https://cdn.tailwindcss.com',
-    'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;700;900&family=Noto+Sans+Arabic:wght@400;700&display=swap'
+    'https://cdn.tailwindcss.com'
 ];
 
+// Install Event
 self.addEventListener('install', (e) => {
+    self.skipWaiting();
     e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(ASSETS);
-        })
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
     );
 });
 
+// Activate Event
+self.addEventListener('activate', (e) => {
+    e.waitUntil(clients.claim());
+});
+
+// Fetch Event
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
-        })
+        caches.match(e.request).then(res => res || fetch(e.request))
     );
 });
